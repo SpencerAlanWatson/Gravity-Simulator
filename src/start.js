e6b77bc8784fd0a58414d3e77fa7cc88b9fd51a9
Spencer Alan Watson <spenceralanwatson@gmail.com>
2014-09-29 08:06:11 -0500

    Update start.js
    I changed it so it would be a more stable orbit.

--- src/start.js
+++ src/start.js
@@ -19,7 +19,7 @@ requirejs(['lib/underscore', 'graphics', 'object', 'geometry/point'],
 
         Graphics.Start(ctx);
         Objects.push(new Object(new Point(250, 250), 20000, new Point(0, 0)));
-        Objects.push(new Object(new Point(350, 250), 200, new Point(0, -2)));
+        Objects.push(new Object(new Point(450, 250), 200, new Point(0, -2)));
         //Objects.push(new Object(new Point(300, 300), 200, new Point(0,-1)));
         window.Stop = Graphics.Stop;
         window.Start = _.partial(Graphics.Start, ctx);
